5a74434a294e5ec06ad3a0fc283e8e3a68d1b294
HappyHTML <75505113+HappyHTML@users.noreply.github.com>
2025-02-13 12:37:43 -0800

    i doubt it

--- src/game.ts
+++ src/game.ts
@@ -11,6 +11,7 @@ class ColorPickerChallenge {
     private colorGridElement: HTMLElement;
     private resultElement: HTMLElement;
     private newGameButton: HTMLElement;
+    private luaGame: any;
 
     constructor() {
         this.targetColorElement = document.getElementById('target-color') as HTMLElement;
@@ -19,44 +20,20 @@ class ColorPickerChallenge {
         this.newGameButton = document.getElementById('new-game') as HTMLElement;
 
         this.newGameButton.addEventListener('click', () => this.startNewGame());
+        this.initLua();
         this.startNewGame();
     }
 
+    private async initLua() {
+        const response = await fetch('game.lua');
+        const luaCode = await response.text();
+        this.luaGame = fengari.load(luaCode)();
+    }
+
     private startNewGame(): void {
-        this.targetColor = this.generateRandomColor();
-        this.options = this.generateColorOptions();
+        this.targetColor = this.luaGame.generateRandomColor();
+        this.options = this.luaGame.generateColorOptions(this.targetColor);
         this.renderGame();
-    }
-
-    private generateRandomColor(): Color {
-        return {
-            r: Math.floor(Math.random() * 256),
-            g: Math.floor(Math.random() * 256),
-            b: Math.floor(Math.random() * 256)
-        };
-    }
-
-    private generateColorOptions(): Color[] {
-        const options: Color[] = [this.targetColor];
-        while (options.length < 6) {
-            const newColor = this.generateRandomColor();
-            if (!options.some(c => this.colorsAreEqual(c, newColor))) {
-                options.push(newColor);
-            }
-        }
-        return this.shuffleArray(options);
-    }
-
-    private colorsAreEqual(c1: Color, c2: Color): boolean {
-        return c1.r === c2.r && c1.g === c2.g && c1.b === c2.b;
-    }
-
-    private shuffleArray<T>(array: T[]): T[] {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
     }
 
     private renderGame(): void {
@@ -74,7 +51,7 @@ class ColorPickerChallenge {
 
     private handleColorClick(index: number): void {
         const selectedColor = this.options[index];
-        if (this.colorsAreEqual(selectedColor, this.targetColor)) {
+        if (this.luaGame.colorsAreEqual(selectedColor, this.targetColor)) {
             this.resultElement.textContent = 'Correct!';
             setTimeout(() => this.startNewGame(), 1500);
         } else {
